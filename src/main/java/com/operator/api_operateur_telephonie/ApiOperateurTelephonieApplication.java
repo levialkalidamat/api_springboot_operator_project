34003b29470e065a906ed1cb7fa64ad608152f1d
Levi alkali damat <105973580+levialkalidamat@users.noreply.github.com>
2023-10-21 03:06:56 +0100

    initial commit

---
+++ src/main/java/com/operator/api_operateur_telephonie/ApiOperateurTelephonieApplication.java
@@ -0,0 +1,13 @@
+package com.operator.api_operateur_telephonie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOperateurTelephonieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOperateurTelephonieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/operator/api_operateur_telephonie/ApiOperateurTelephonieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operator.api_operateur_telephonie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOperateurTelephonieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
